1d0a13ebe45c61b465dc727328848c1829aa1a31
BenWol <benjamin.wolter.23@gmail.com>
2018-08-21 22:36:36 +0200

    initial commit of the cookiecutter folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project intends to built a prediction environment that forecasts the goals of all football matches on any new match day like e.g. of the Bundesliga. Key here is to not just predict who wins and loses but to predict the goals of the home and the away team with highest accuracy possible.',
+    author='Benjamin Wolter',
+    license='MIT',
+)
